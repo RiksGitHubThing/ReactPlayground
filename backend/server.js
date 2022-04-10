76df9ffc222a7e457bf82af2d33a9b9bebe56b53
Mr Waster <riksAWSmail@gmail.com>
2022-04-11 00:29:29 +0100

    Set up controllers, errors and routes

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,14 @@
-console.log('hello world')
+const express = require('express')
+const dotenv = require('dotenv').config()
+
+const {errorHandler} = require('./middleware/errorMiddleware')
+
+const port = process.env.PORT || 5000
+
+const app = express()
+
+app.use('/api/goals', require('./routes/goalRoutes'))
+
+app.use(errorHandler)
+
+app.listen(port, () => console.debug(`Server started on port ${port}`))
--- readme.md
+++ readme.md
@@ -3,3 +3,5 @@ Stuff to remember - backend
 npm i express dotenv mongoose colors
 
 npm i -D nodemon
+
+npm i express-async-hander
